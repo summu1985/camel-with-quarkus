113c205158f89bcd26a0845bd0e92ce0631b4569
Sumit Mukherjee <sumit.mckv@gmail.com>
2025-01-17 17:43:19 +0530

    Modified code to take clientid and client secret from input

--- src/main/java/com/redhat/demo/camelquarkus/Routes.java
+++ src/main/java/com/redhat/demo/camelquarkus/Routes.java
@@ -4,8 +4,6 @@ import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 
-
-
 public class Routes extends RouteBuilder{
 
     @Override
@@ -13,24 +11,34 @@ public class Routes extends RouteBuilder{
         // TODO Auto-generated method stub
         restConfiguration().bindingMode(RestBindingMode.json);
         rest("/token")
-                .post()
+                .post().type(input.class)
                 .to("direct:getToken");
 
         from("direct:getToken")
         .log("Recieved request with body : ${body}")
-        .to("log:DEBUG?showBody=true&showHeaders=true")
+        //.convertBodyTo(String.class)
+        //.unmarshal(new JacksonDataFormat(input.class))
         .removeHeaders("*")
+        .process(exchange->{
+            input customerInput = exchange.getIn().getBody(input.class);
+            exchange.getIn().setHeader("client_id", customerInput.getClientid());
+            exchange.getIn().setHeader("client_secret", customerInput.getClientsecret());
+        })
+        //.to("log:DEBUG?showBody=true&showHeaders=true")
         .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-            .setHeader(Exchange.CONTENT_TYPE, constant("application/x-www-form-urlencoded"))
+        .setHeader(Exchange.CONTENT_TYPE, constant("application/x-www-form-urlencoded"))
             // ensure that you use correct client if and client secret
-            .setBody(simple("grant_type=client_credentials&client_id={{client.id}}&client_secret={{client.secret}}"))
+            //.setBody(simple("grant_type=client_credentials&client_id={{client.id}}&client_secret={{client.secret}}"))
+        .setBody(simple("grant_type=client_credentials&client_id=${header.client_id}&client_secret=${header.client_secret}"))
             // Change the below url to your keycloak token endpoint
-            .toD("{{sso.token.endpoint}}")
+            // show the body
+        //.log("${body}")
+        .toD("{{sso.token.endpoint}}")
             //.unmarshal().base64().log("${body}");
-            .convertBodyTo(String.class)
+        .convertBodyTo(String.class)
             //.marshal().json()
-            .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-            .to("log:DEBUG?showBody=true&showHeaders=true");
+        .setHeader(Exchange.CONTENT_TYPE, constant("application/json"));
+        //.to("log:DEBUG?showBody=true&showHeaders=true");
         //.setBody(constant("Hello from Quarkus"));
     }
     
---
+++ src/main/java/com/redhat/demo/camelquarkus/input.java
@@ -0,0 +1,21 @@
+package com.redhat.demo.camelquarkus;
+
+public class input {
+    private String clientid;
+    private String clientsecret;
+    public String getClientid() {
+        return clientid;
+    }
+    public void setClientid(String clientid) {
+        this.clientid = clientid;
+    }
+    public String getClientsecret() {
+        return clientsecret;
+    }
+    public void setClientsecret(String clientsecret) {
+        this.clientsecret = clientsecret;
+    }
+
+
+    
+}
